fa4cb35e1648f7fbf86724a752a89f9e6f58e0bf
dmytromayevsky <mayevskydv@gmail.com>
2024-09-05 18:00:54 +0200

    Initial Commit

---
+++ Daily Journal/Daily_JournalApp.swift
@@ -0,0 +1,17 @@
+//
+//  Daily_JournalApp.swift
+//  Daily Journal
+//
+//  Created by Dmytro Mayevsky on 05.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct Daily_JournalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
